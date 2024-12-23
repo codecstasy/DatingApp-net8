e540bed4798a107ea4134c837507025cd33fc685
shattikbandyopadhyaa <shattik.bandyopadhyaa@orbitax.com>
2024-12-23 18:38:19 +0600

    Mid 46

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+  model: any = {};
+
+  login() {
+    console.log(this.model);
+  }
+}
